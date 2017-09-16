903e982279cff096758cb8872797683f77cdf78b
Katsuhisa Maruyama <maru@cs.ritsumei.ac.jp>
2017-09-16 14:07:50 +0900

    Added comments

--- org.jtool.macrorecorder.sample/http_server/http.js
+++ org.jtool.macrorecorder.sample/http_server/http.js
@@ -1,23 +1,29 @@
+// 
+// A simple server for testing HTTP POST requests
+// This program for node.js. To run,
+//   % node http.js
+//
+
 var http = require('http');
 var querystring = require('querystring');
 
 http.createServer(function (req, res) {
-
+    
     if (req.url === '/' && req.method === 'GET') {
-    	res.writeHead(200, {'Content-Type': 'text/plain'});
-	res.end('This is a test HTTP server!\n');
-
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('This is a test HTTP server!\n');
+    
     } else if (req.url === '/post' && req.method === 'POST') {
-	var data = '';
-	req.on('readable', function(chunk) {
-	    data += req.read();
-	});
-	req.on('end', function() {
-	    querystring.parse(data);
+    var data = '';
+    req.on('readable', function(chunk) {
+        data += req.read();
+    });
+    req.on('end', function() {
+        querystring.parse(data);
             res.end(data);
             console.log(data);
         });
-
+    
     } else {
         res.statusCode = 404;
         res.end('Not Found!\n');
