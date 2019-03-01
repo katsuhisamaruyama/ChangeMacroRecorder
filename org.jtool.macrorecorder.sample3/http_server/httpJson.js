a5f6eb0b267868047595c184418ae7ff12b2a4ea
Katsuhisa Maruyama <maru@cs.ritsumei.ac.jp>
2019-03-01 13:02:17 +0900

    Server program was updated

---
+++ org.jtool.macrorecorder.sample3/http_server/httpJson.js
@@ -0,0 +1,32 @@
+// 
+// A simple server for testing HTTP POST requests
+// This program for node.js. To run,
+//   % node http.js
+
+const http = require('http');
+const port = 1337;
+const querystring = require('querystring');
+
+const server = http.createServer(function (request, response) {
+    request.setEncoding("utf-8");
+
+    if (request.method === 'GET') {
+        response.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8'});
+        response.end('This is a ChangeMacroRecorder HTTP server!\n');
+
+    } else if (request.method === 'POST') {
+
+        request.on("data", function(chunk) {
+            const data = JSON.parse(chunk);            
+            console.log(data);            
+            response.statusCode = 200;
+            response.end(JSON.stringify({ 'result': 'Ok' }));
+        });
+    
+    } else {
+        respond.statusCode = 404;
+        response.end(JSON.stringify({ 'result': 'Not Found!' }));
+    }
+});
+server.listen(1337, '127.0.0.1');
+console.log('Server running');
